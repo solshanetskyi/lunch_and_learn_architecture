cc0a1b6d4407d9c8febe38fe339ef55d6aad4848
Sergii Olshanetskyi <solshanetskyi@freshbooks.com>
2020-05-19 21:12:04 -0400

    Adding more tests.

--- src/tests/test_model.py
+++ src/tests/test_model.py
@@ -1,9 +1,10 @@
+from datetime import date
 from decimal import Decimal
 
-from src.domain.model import InvoiceProfileLine, InvoiceProfileLineType
+from src.domain.model import InvoiceProfileLine, InvoiceProfileLineType, InvoiceProfile, Schedule, ScheduleType
 
 
-def test_amount_without_taxes():
+def test_line_item_amount_without_taxes():
     line: InvoiceProfileLine = InvoiceProfileLine(name='Fake name',
                                                   description='Fake Description',
                                                   unit_cost=Decimal(4.0),
@@ -13,7 +14,7 @@ def test_amount_without_taxes():
     assert 24 == line.amount
 
 
-def test_amount_wit_taxes():
+def test_line_item_amount_wit_taxes():
     line: InvoiceProfileLine = InvoiceProfileLine(name='Fake name',
                                                   description='Fake Description',
                                                   unit_cost=Decimal(100.0),
@@ -25,3 +26,29 @@ def test_amount_wit_taxes():
     assert Decimal(113) == line.amount
 
 
+def test_profile_amount():
+    line_1: InvoiceProfileLine = InvoiceProfileLine(name='Fake name',
+                                                    description='Fake Description',
+                                                    unit_cost=Decimal(5.0),
+                                                    quantity=6,
+                                                    type=InvoiceProfileLineType.Simple)
+
+    line_2: InvoiceProfileLine = InvoiceProfileLine(name='Fake name',
+                                                    description='Fake Description',
+                                                    unit_cost=Decimal(4.0),
+                                                    quantity=3,
+                                                    type=InvoiceProfileLineType.Simple)
+
+    profile = InvoiceProfile(lines=[line_1, line_2], client_id=3, schedule=None)
+
+    assert 42 == profile.amount
+
+
+def test_schedule_next_date():
+    schedule = Schedule(
+        start_date=date(2020, 1, 1),
+        last_occurrence_date=date(2020, 1, 1),
+        schedule_type=ScheduleType.Weekly,
+        schedule_multiplier=2)
+
+    assert schedule.next_occurrence_date == date(2020, 1, 15)
